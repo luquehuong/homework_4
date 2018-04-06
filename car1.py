0178e4fc01e389d3d4cbba7b44c7f105776f95ac
Vy Hong <contact@vyhong.me>
2018-04-05 22:28:40 -0700

    Successfully draw car body.

--- car1.py
+++ car1.py
@@ -1,19 +1,27 @@
-from graphics import Point
+from graphics import Point, Rectangle, GraphWin
 
 class Car():
     def __init__(self, back_wheel_center, back_wheel_radius, front_wheel_center, front_wheel_radius, body_height):
-        pass
+        upper_left_point = Point(back_wheel_center.x, back_wheel_center.y - body_height)
+        bottom_right_point = front_wheel_center
+        self.body = Rectangle(upper_left_point, bottom_right_point)
 
-    def draw(win):
-        pass
+    def set_color(self, tire_color, wheel_color, body_color):
+        self.body.setFill(body_color)
 
-    def animate(win, dx, dy, n):
-        pass
+    def draw(self, win):
+        self.body.draw(win)
 
-    def set_color(tire_color, wheel_color, body_color):
+    def animate(self, win, dx, dy, n):
         pass
 
 def main():
+    win = GraphWin("An Awesome Car", 700, 300)
+
     car = Car(Point(50, 50), 15, Point(100,50), 15, 40)
+    car.set_color('black', 'gray', 'pink')
+    car.draw(win)
+
+    win.mainloop()
 
 main()

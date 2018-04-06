6c5cfa04570d4a1e98f0116078ea8b81b2842fb0
Vy Hong <contact@vyhong.me>
2018-04-05 22:17:37 -0700

    Setup skeleton for Car class.

---
+++ car1.py
@@ -0,0 +1,19 @@
+from graphics import Point
+
+class Car():
+    def __init__(self, back_wheel_center, back_wheel_radius, front_wheel_center, front_wheel_radius, body_height):
+        pass
+
+    def draw(win):
+        pass
+
+    def animate(win, dx, dy, n):
+        pass
+
+    def set_color(tire_color, wheel_color, body_color):
+        pass
+
+def main():
+    car = Car(Point(50, 50), 15, Point(100,50), 15, 40)
+
+main()
